ccf35b661678a74934459528b2c467ac8abf7b5f
ratnakar dashrath yadav <117161937+ratn7921@users.noreply.github.com>
2024-08-13 18:27:39 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,50 +1,51 @@
-import bannerImage from '../assets/6644b34c91f57f8d40a4eaa94e3cb797 - Copy.png'
-import bannerBackground from '../assets/pattern-background-mobile.svg'
-const Banner=()=>{
-    return(
-      <div  style={
-        {backgroundImage:`url(${bannerBackground})`,
-        backgroundSize:"cover",
-       }   
-       }  className="main-containt flex items-center" >
-     
-        <div className='mt-6 w-full flex h-screen justify-center'>
-<div className="mt-6 w-2/3 space-y-2 ms-10">
-<h3 className="mt-6 text-3xl font-semibold ">Hi, I am</h3>
-<h1 className="mt-6 text-5xl  font-bold   ">Ratnakar Dashrath Yadav</h1>
-<h2 className=' text-3xl'>I am frontend developer</h2>
-<p className="mt-3 ">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-     Sunt rem itaque inventore perspiciatis,explicabo harum! 
-     Aliquid, quia facere corrupti tempora debitis quis veritatis voluptate optio,
-     cum aliquam tenetur cumque nihil.
-</p>
-<div className="icon-container    space-x-6">
-    <a className="bg-black hover:bg-white   cursor-pointer px-4 py-2 w-6 h-6 rounded-full " href="#"><i class="fa-brands hover:text-black text-white fa-linkedin-in"></i>
-    </a> 
-    <a className="bg-black hover:bg-white  cursor-pointer px-4 py-2 w-6 h-6 rounded-full " href="#"><i class="fa-brands hover:text-black text-white fa-twitter"></i>
-    </a>
-    <a className="bg-black hover:bg-white  cursor-pointer px-4 py-2 w-6  h-6 rounded-full " href="#"><i class="fa-brands hover:text-black text-white fa-github"></i>
-    </a>
-<a className="bg-black hover:bg-white  cursor-pointer px-4 py-2 w-6 h-6 rounded-full " href="#"><i class="fa-brands hover:text-black text-white fa-instagram"></i>
-</a>
-</div>
-<br />
-<a className="mt-18 shadow-lg rounded-full bg-yellow-300 px-3 py-2" href="/contact"> Contact me</a>
-<h1 className=" ">
-     
+import bannerImage from '../assets/6644b34c91f57f8d40a4eaa94e3cb797 - Copy.png';
+import bannerBackground from '../assets/pattern-background-mobile.svg';
 
-</h1>
-</div>
-
+const Banner = () => {
+  return (
+    <div 
+      className="main-containt flex items-center h-screen" 
+      style={{
+        backgroundImage: `url(${bannerBackground})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className='flex w-full justify-center'>
+        <div className="mt-6 w-2/3 space-y-4 ms-10">
+          <h3 className="text-3xl font-semibold">Hi, I am</h3>
+          <h1 className="text-5xl font-bold">Ratnakar Dashrath Yadav</h1>
+          <h2 className='text-3xl'>I am a Frontend Developer</h2>
+          <p className="mt-3 text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt rem itaque inventore perspiciatis, explicabo harum! 
+            Aliquid, quia facere corrupti tempora debitis quis veritatis voluptate optio, cum aliquam tenetur cumque nihil.
+          </p>
+          <div className="icon-container flex space-x-6">
+            <a className="bg-black hover:bg-white cursor-pointer p-2 w-10 h-10 rounded-full flex items-center justify-center" href="#">
+              <i className="fa-brands fa-linkedin-in text-white hover:text-black"></i>
+            </a> 
+            <a className="bg-black hover:bg-white cursor-pointer p-2 w-10 h-10 rounded-full flex items-center justify-center" href="#">
+              <i className="fa-brands fa-twitter text-white hover:text-black"></i>
+            </a>
+            <a className="bg-black hover:bg-white cursor-pointer p-2 w-10 h-10 rounded-full flex items-center justify-center" href="#">
+              <i className="fa-brands fa-github text-white hover:text-black"></i>
+            </a>
+            <a className="bg-black hover:bg-white cursor-pointer p-2 w-10 h-10 rounded-full flex items-center justify-center" href="#">
+              <i className="fa-brands fa-instagram text-white hover:text-black"></i>
+            </a>
+          </div>
+          <a className="mt-6 inline-block shadow-lg rounded-full bg-yellow-300 px-5 py-3 text-black font-semibold" href="/contact">Contact me</a>
         </div>
 
-   <div className=" mt-6 w-full flex h-screen justify-center ">
-    {/* image */}
-    <img className=" size-6/12 rounded-full shadow-inner bg-sky-200   px-5 py-6   "  src={bannerImage} />
+        <div className="mt-6 w-full flex justify-center">
+          <img 
+            className="w-6/12 rounded-full shadow-inner bg-sky-200 p-6" 
+            src={bannerImage} 
+            alt="Ratnakar Dashrath Yadav" 
+          />
+        </div>
+      </div>
     </div>
-             
-         </div>
-    );
+  );
 };
+
 export default Banner;
