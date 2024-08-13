52963921d604db6d71270b5632cd0d23c108cd5d
ratnakar dashrath yadav <117161937+ratn7921@users.noreply.github.com>
2024-08-13 15:08:13 +0530

    buildnolatest

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/myportfolio/',
-  build:{
-    outDir: "./docs",
+  base: '/myportfolio/', // Base URL for your project
+  build: {
+    outDir: './docs', // Output directory for built files
   },
 });
